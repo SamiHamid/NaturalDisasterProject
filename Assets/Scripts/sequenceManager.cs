6a1e756e503bb6f29f7ea2daa11018dc6b657ec1
asmccormick <asmccormick@gmail.com>
2016-03-26 15:22:25 +0200

    camera shake during quake
    need to switch transform to Vive camera; currently it is the player
    controller

--- Assets/Scripts/sequenceManager.cs
+++ Assets/Scripts/sequenceManager.cs
@@ -7,6 +7,11 @@ public class sequenceManager : MonoBehaviour {
 	private Text _tvText;
 	private Text _timerText;
 	private string _timeString;
+	public Transform _cameraToShake;
+	public bool _shakeCamera; // set to private after testing
+	private float _shakeStartTime;
+	public float _shakeDuration;
+	public float _shakeMaxMovement;
 
 	void Start () {
 		_tvText = GameObject.Find("Dynamic GUI/TV Text").GetComponent<Text>();
@@ -16,8 +21,13 @@ public class sequenceManager : MonoBehaviour {
 	
 
 	void Update () {
+		// update time on TV screen
 		_timeString = string.Format("{0:0}:{1:00}", Mathf.Floor(Time.time/60), Time.time % 60);
 		_timerText.text = _timeString;
+
+		if (_shakeCamera) {
+			ShakeCamera();
+		}
 	}
 
 	public void NewItemCollected (string itemName) {
@@ -31,6 +41,8 @@ public class sequenceManager : MonoBehaviour {
 
 		if (_tvText.text == "green cube" && itemName == "green cube") {
 			_tvText.text = "go under table";
+			_shakeCamera = true;
+			_shakeStartTime = Time.time;
 			gameObject.GetComponent<AudioSource>().Play();
 		}
 	}
@@ -48,5 +60,17 @@ public class sequenceManager : MonoBehaviour {
 	IEnumerator PackGreenCube () {
 		_tvText.text = "green cube";
 		yield return null;
+	}
+
+	private void ShakeCamera () {
+
+		// add some equation that converts shake magnitude to a parabola
+
+		float _newX = _cameraToShake.position.x + Random.Range(-0.01f, 0.01f);
+		_cameraToShake.position = new Vector3 (_newX, _cameraToShake.position.y, _cameraToShake.position.z);
+
+		if (Time.time > _shakeStartTime + _shakeDuration) {
+			_shakeCamera = false;
+		}
 	}
 }
