0d326abe1897a94dd94558d482b0446aa921e646
asmccormick <asmccormick@gmail.com>
2016-03-26 11:45:16 +0200

    basic packing sequence

---
+++ Assets/Scripts/firstAidBag.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class firstAidBag : MonoBehaviour {
+
+	private sequenceManager _sequenceManager;
+
+	// Use this for initialization
+	void Start () {
+		_sequenceManager = GameObject.Find("Sequence Manager").GetComponent<sequenceManager>();
+		Debug.Log("sequenceManager = " + _sequenceManager);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.gameObject.tag == "FirstAidItem") {
+			Destroy (collision.gameObject);
+			_sequenceManager.NewItemCollected(collision.gameObject.name);
+		}
+	}
+}
---
+++ Assets/Scripts/sequenceManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class sequenceManager : MonoBehaviour {
+
+	private Text _tvText;
+	private Text _timerText;
+	private string _timeString;
+
+	void Start () {
+		_tvText = GameObject.Find("Dynamic GUI/TV Text").GetComponent<Text>();
+		_timerText = GameObject.Find("Dynamic GUI/Timer Text").GetComponent<Text>();
+		StartCoroutine(PackRedCube());
+	}
+	
+
+	void Update () {
+		_timeString = string.Format("{0:0}:{1:00}", Mathf.Floor(Time.time/60), Time.time % 60);
+		_timerText.text = _timeString;
+	}
+
+	public void NewItemCollected (string itemName) {
+		if (_tvText.text == "red cube" && itemName == "red cube") {
+			StartCoroutine(PackBlueCube());
+		}
+
+		if (_tvText.text == "blue cube" && itemName == "blue cube") {
+			StartCoroutine(PackGreenCube());
+		}
+
+		if (_tvText.text == "green cube" && itemName == "green cube") {
+			_tvText.text = "go under table";
+			gameObject.GetComponent<AudioSource>().Play();
+		}
+	}
+
+	IEnumerator PackRedCube () {
+		_tvText.text = "red cube";
+		yield return null;
+	}
+
+	IEnumerator PackBlueCube () {
+		_tvText.text = "blue cube";
+		yield return null;
+	}
+
+	IEnumerator PackGreenCube () {
+		_tvText.text = "green cube";
+		yield return null;
+	}
+}
