ec8d226f8e994914fbf59a2656191878b76aaf35
SamiHamid <samihamid@hotmail.co.uk>
2016-04-07 20:59:43 +0300

    Earthquake Sequence + Interactable effects

--- Assets/Scripts/firstAidBag.cs
+++ Assets/Scripts/firstAidBag.cs
@@ -7,10 +7,17 @@ public class firstAidBag : MonoBehaviour {
 	public AudioSource _sfx;
 	public GameObject ParticleSuccess;
 	public GameObject ParticleSpawn;
+	public GameObject ParticleFailure;
+
+	//Audio
+	private AudioSource Success;
+	private AudioSource Failure;
 
 
 	void Start () {
 		_sequenceManager = GameObject.Find("Sequence Manager").GetComponent<sequenceManager>();
+		Success = GameObject.Find("SuccessSound").GetComponent<AudioSource>();
+		Failure = GameObject.Find("FailureSound").GetComponent<AudioSource>();
 	}
 	
 
@@ -19,12 +26,21 @@ public class firstAidBag : MonoBehaviour {
 	}
 
 
-	void OnCollisionEnter (Collision collision) {
+	void OnCollisionEnter (Collision collision) 
+	{
 		if (collision.gameObject.tag == "FirstAidItem") {
 			Destroy (collision.gameObject);
 			_sequenceManager.NewItemCollected(collision.gameObject.name);
-			_sfx.Play ();
 			Instantiate (ParticleSuccess,ParticleSpawn.transform.position, Quaternion.identity);
+			Success.Play();
+		}
+
+		// DROPPING WRONG ITEM IN BAG
+		if (collision.gameObject.tag == "WrongItem")
+		{
+			Destroy (collision.gameObject);
+			Instantiate (ParticleFailure,ParticleSpawn.transform.position, Quaternion.identity);
+			Failure.Play();
 		}
 	}
 }
