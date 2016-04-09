efbbf4fec6bf642f98a473a2cb840fba65f14700
asmccormick <asmccormick@gmail.com>
2016-04-09 15:29:39 -0400

    Revert "red circle under table"
    This reverts commit 3be4753a71a865c63ae2ceffb0be6557a7cb9fa1.

--- Assets/Scripts/sequenceManager.cs
+++ Assets/Scripts/sequenceManager.cs
@@ -17,9 +17,8 @@ public class sequenceManager : MonoBehaviour {
     private EarthquakeController _earthquakeController;
 	private Transform _timerRenderer;
 	public bool _quakeHasStarted;				//may not need to be pulic
-	private GameObject _underTableBullseye;
 
-	// Audio for  TV
+	// Audio for the TV
 	private AudioSource _tvAudioSource;
 	public AudioClip warning;
 	public AudioClip intro;
@@ -36,10 +35,6 @@ public class sequenceManager : MonoBehaviour {
 	public AudioClip scissors;
 	public AudioClip triangularBandage;
 
-	public AudioClip getUnderTable;
-	// end of Audio for TV
-
-
 	// Audio for Hammer Sequence
 	public AudioClip hammerIntro;
 	public AudioClip target1done;
@@ -49,8 +44,13 @@ public class sequenceManager : MonoBehaviour {
 	public AudioClip bracket2done;
 
 
-	// Textures for the TV
+	public AudioClip getUnderTable;
+
+
 	// New Order: roll, alc, cpr v, manual, band, tri, pins, scissors
+
+
+	// Textures for the TV
 	public Material alcoholWipesImg;
 	public Material bandagesImg;
 	public Material firstAidBookImg;
@@ -86,9 +86,6 @@ public class sequenceManager : MonoBehaviour {
 		_hammerTarget3.SetActive(false);
 		_hammerTarget4.SetActive(false);
 
-		_underTableBullseye = GameObject.Find("Under Table Bullseye");
-		_underTableBullseye.SetActive(false);
-
 		//_timerRenderer = GameObject.Find("Timer Text").GetComponent<Renderer>();
 		_timerRenderer = GameObject.Find("Timer Text").GetComponent<Transform>();
 		Debug.Log("timerR = " + _timerRenderer);
@@ -117,7 +114,6 @@ public class sequenceManager : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
             StopAllCoroutines();		//Mert says we need this
-			StartCoroutine(DropCoverHold());
 			_earthquakeController.StartQuake();
 		}
 
@@ -247,17 +243,17 @@ public class sequenceManager : MonoBehaviour {
 		_tvAudioSource.Play();
 	}
 
-
+	/*
 	IEnumerator DropCoverHold () {
 		_tvText.text = "";
 		_tvImage.material = dropCoverHoldImg;
 		_tvAudioSource.clip = getUnderTable;  // use the longer clip with "get under... hold on... hold on..."
 		_tvAudioSource.Play();
-		_underTableBullseye.SetActive(true);
 		//yield return new WaitForSeconds(5);
-		//_earthquakeController.StartQuake();
+		_earthquakeController.StartQuake();
 		yield return null;
 	}
+	*/
 		
 	public void NextHammerTarget (int nextStep) {
 		if (nextStep == 2) {
